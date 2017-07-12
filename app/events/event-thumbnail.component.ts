a82befc74b7f9c71fbac9a17e23a01f9a5cc75ca
ironjb <ironjb20@gmail.com>
2017-07-11 18:05:12 -0600

    added IEvent interface and added some login stuff

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared/index';
 
 @Component({
 	selector: 'event-thumbnail'
@@ -12,7 +13,7 @@ import { Component, Input } from '@angular/core';
 	`]
 })
 export class EventThumbnailComponent {
-	@Input() event: any;
+	@Input() event: IEvent;
 
 	getStartTimeClass() {
 		// // Return Object

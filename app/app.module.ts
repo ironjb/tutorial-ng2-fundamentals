19987ef5f7ae2c2b0ce0e0ac98c49570128a5720
ironjb <ironjb20@gmail.com>
2017-07-12 01:58:56 -0600

    Login, validation, create event

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {
 	EventsListComponent
@@ -12,14 +13,17 @@ import {
 	, EventListResolver
 } from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { NavBarComponent } from "./nav/navbar.component";
-import { ToastrService } from "./common/toastr.service";
-import { appRoutes } from "./routes";
-import { Error404Component } from "./errors/404.component";
+import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
 	imports: [
 		BrowserModule
+		, FormsModule
+		, ReactiveFormsModule
 		, RouterModule.forRoot(appRoutes)
 	]
 	, declarations: [
@@ -36,6 +40,7 @@ import { Error404Component } from "./errors/404.component";
 		, ToastrService
 		, EventRouteActivator
 		, EventListResolver
+		, AuthService
 		, { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
 	]
 	, bootstrap: [EventsAppComponent]

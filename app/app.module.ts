a76cc7c7c3c953dc0f664e347812e81112dc0cee
ironjb <ironjb20@gmail.com>
2017-07-13 17:16:48 -0600

    12. Creating Directives and Advanced Components in Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,13 +17,20 @@ import {
 } from './events/index';
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
-import { CollapsibleWellComponent } from './common/collapsible-well.component';
+import {
+	TOASTR_TOKEN
+	, Toastr
+	, JQ_TOKEN
+	, CollapsibleWellComponent
+	, SimpleModalComponent
+	, ModalTriggerDirective
+} from './common/index';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 
 declare let toastr: Toastr;
+declare let jQuery: Object;
 
 @NgModule({
 	imports: [
@@ -44,6 +51,8 @@ declare let toastr: Toastr;
 		, SessionListComponent
 		, CollapsibleWellComponent
 		, DurationPipe
+		, SimpleModalComponent
+		, ModalTriggerDirective
 	]
 	, providers: [
 		EventService
@@ -52,6 +61,7 @@ declare let toastr: Toastr;
 		, AuthService
 		, { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
 		, { provide: TOASTR_TOKEN, useValue: toastr }
+		, { provide: JQ_TOKEN, useValue: jQuery }
 	]
 	, bootstrap: [EventsAppComponent]
 })

e643ad987c5bc5926d1fca3b3f0b2e0fb611ccc4
ironjb <ironjb20@gmail.com>
2017-07-13 12:10:03 -0600

    10. Displaying Data with Pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,6 +13,7 @@ import {
 	, EventListResolver
 	, CreateSessionComponent
 	, SessionListComponent
+	, DurationPipe
 } from './events/index';
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
@@ -40,6 +41,7 @@ import { AuthService } from './user/auth.service';
 		, CreateSessionComponent
 		, SessionListComponent
 		, CollapsibleWellComponent
+		, DurationPipe
 	]
 	, providers: [
 		EventService
